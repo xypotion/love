70bdf8959ed0decddacfb01f7ec05e17e6cffad4
Max W <xypotion@gmail.com>
2014-07-24 00:00:03 -0700

    separated hero-navigation functions to new file, massively refactored map.lua; everything still works :)

--- explore3/main.lua
+++ explore3/main.lua
@@ -1,4 +1,5 @@
 require "map"
+require "hero"
 
 function love.load()
   love.window.setTitle('Löaf 2D')
@@ -22,7 +23,7 @@ function love.update(dt)
 	animateHero(dt)
 
 	-- move map if needed
-	if screenShiftingDirection then
+	if screenShifting then
 		shiftTiles(dt)
 	end
 	
@@ -31,7 +32,7 @@ function love.update(dt)
 		shiftHero(dt * heroShiftSpeed)
 	end
 	
-	if not screenShiftingDirection and not heroShifting then --simplify? 
+	if not screenShifting and not heroShifting then --simplify? 
 		-- allow player to move hero
 		setHeroGridTargetAndTileTypeIfDirectionKeyPressed()
 		heroGo()
@@ -40,13 +41,12 @@ end
 
 function love.draw()
 	drawBGTiles()
-	
 	drawHero()
 	
   love.graphics.setColor(255, 255, 255, 255)
   love.graphics.print("Current FPS: "..tostring(love.timer.getFPS()), 10, 10)
   love.graphics.print("SCORE: "..score, 10, 26)
-	love.graphics.print(" x="..worldX.." y="..worldY, tileSize * xLen - 96, 10)
+	love.graphics.print(" x="..worldPos[1].." y="..worldPos[2], tileSize * xLen - 96, 10)
 	love.graphics.print(" x="..heroGridPos[1].." y="..heroGridPos[2], tileSize * xLen - 96, 26)
 end
 
@@ -59,37 +59,19 @@ end
 
 ------------------------------------------------------------------------------------------------------
 
-function initHero()
-	heroImage = love.graphics.newImage("man7d.png")
-	heroQuads = {
-		love.graphics.newQuad(0,0,32,32,64,32),
-		love.graphics.newQuad(32,0,32,32,64,32)
-	}
-	
-	heroFrameLength = .4
-	heroTime = 0
-	heroSpriteState = 1
-	
-	-- heroGridPos = {7,7} --x,y
-	heroGridPos = {2,2}
-	heroGridTarget = heroGridPos
-	setHeroXY()
-	
-	heroWalkSpeed = 200
-end
-
 function tileType(xy)
 	type = "clear"
 
 	if xy[1] == xLen + 1 then
-		type = "east edge" --trying to go beyond right edge
+		--somewhat redundant as these values are translated immediately in heroGo() to the more useful worldDest. fine for now, though
+		type = "east edge"
 	elseif xy[2] == yLen + 1 then
-		type = "south edge" --trying to go beyond bottom
+		type = "south edge"
 	else
 		if not currentMap[xy[2]] then
-			type = "north edge" --nothing at currentMap[0]
+			type = "north edge"
 		elseif not currentMap[xy[2]][xy[1]] then
-			type = "west edge" --nothing at currentMap[y][0]
+			type = "west edge"
 		else
 			if currentMap[xy[2]][xy[1]] == 1 then
 				type = "collide"
@@ -98,92 +80,6 @@ function tileType(xy)
 	end
 	
 	return type
-end
-
-function shiftHero(speed)
-	xDelta = (heroGridTarget[1] - heroGridPos[1]) * speed
-	yDelta = (heroGridTarget[2] - heroGridPos[2]) * speed
-	
-	heroX = heroX + xDelta
-	heroY = heroY + yDelta
-	
-	heroDistanceFromTarget = heroDistanceFromTarget - math.abs(xDelta + yDelta)
-	
-	if heroDistanceFromTarget <= 0 then
-		heroArrive()
-		setHeroGridTargetAndTileTypeIfDirectionKeyPressed()
-		heroGo()
-	end
-end
-
-function setHeroGridTargetAndTileTypeIfDirectionKeyPressed()
-	--someday make the LAST-PRESSED key be the direction the hero moves, allowing many to be pressed at once? lock others until keyReleased()? hm
-	numKeysPressed = 0
-	
-	if love.keyboard.isDown('d') then
-		heroGridTarget = {heroGridPos[1] + 1, heroGridPos[2]}
-		numKeysPressed = numKeysPressed + 1
-	end
-	if love.keyboard.isDown('a') then
-		heroGridTarget = {heroGridPos[1] - 1, heroGridPos[2]}
-		numKeysPressed = numKeysPressed + 1
-	end
-	if love.keyboard.isDown('w') then
-		heroGridTarget = {heroGridPos[1], heroGridPos[2] - 1}
-		numKeysPressed = numKeysPressed + 1
-	end
-	if love.keyboard.isDown('s') then
-		heroGridTarget = {heroGridPos[1], heroGridPos[2] + 1}
-		numKeysPressed = numKeysPressed + 1
-	end
-	
-	-- too many keys; never mind!
-	if numKeysPressed > 1 then
-		heroGridTarget = heroGridPos
-	end
-
-	-- get & set type
-	if not (heroGridTarget == heroGridPos) then
-		targetTileType = tileType(heroGridTarget)
-	end
-end
-
-function heroGo()
-	if targetTileType == "clear" then
-		heroShifting = true
-		heroShiftSpeed = heroWalkSpeed
-		heroDistanceFromTarget = tileSize
-	elseif targetTileType == "collide" then -- for now...
-		-- sound effect or something
-	elseif targetTileType and string.find(targetTileType, "edge") then
-		--gotta change that target!
-		heroGridTarget = {(heroGridTarget[1] - 1) % xLen + 1, (heroGridTarget[2] - 1) % yLen + 1}
-		
-		heroShifting = true
-		if heroGridTarget[1] == heroGridPos[1] then
-			heroShiftSpeed = scrollSpeed / yLen
-			heroDistanceFromTarget = (yLen - 1) * tileSize
-		elseif heroGridTarget[2] == heroGridPos[2] then
-			heroShiftSpeed = scrollSpeed / xLen
-			heroDistanceFromTarget = (xLen - 1) * tileSize
-		else
-			print("something has gone very wrong in heroGo()")
-		end
-		
-		triggerScreenShift(targetTileType)
-	end
-end
-
-function heroArrive()
-	heroDistanceFromTarget = 0
-	heroShifting = false
-	targetTileType = nil
-	
-	--finalize and snap to grid
-	heroGridPos = heroGridTarget
-	setHeroXY()
-	
-	arrivalInteraction()
 end
 
 function arrivalInteraction()
@@ -196,24 +92,6 @@ function arrivalInteraction()
 		currentMap[heroGridPos[2]][heroGridPos[1]] = 0
 		updateTilesetBatchCurrent()
 	end
-end
-
-function setHeroXY()
-	heroX = (heroGridPos[1] - 1) * tileSize + xMargin
-	heroY = (heroGridPos[2] - 1) * tileSize + yMargin
-end
-
-function animateHero(dt)
-	-- could actually see setting the current quad in here to simplify drawHero(), especially after implementing directional sprites
-	heroTime = heroTime + dt
-	if heroTime > heroFrameLength then
-		heroSpriteState = (heroSpriteState + 1) % 2
-		heroTime = 0
-	end
-end
-
-function drawHero()
-	love.graphics.draw(heroImage, heroQuads[heroSpriteState + 1], heroX, heroY)
 end
 
 function love.quit()
--- explore3/map.lua
+++ explore3/map.lua
@@ -15,30 +15,28 @@ function initTileSystem()
 	-- starting tiles
 	currentMap = 
 	{
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		-- {0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0},
-		-- {0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0},
-		-- {0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0},
-		-- {0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0},
-		-- {0,0,1,1,0,0,0,0,0,0,0,1,1,1,0,0},
-		-- {0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0},
-		-- {0,1,1,1,1,0,0,2,2,0,1,1,1,1,0,0},
-		-- {0,1,0,0,1,0,0,2,2,0,0,0,0,1,0,0},
-		-- {0,1,0,0,1,0,0,0,0,0,1,1,1,0,0,0},
-		-- {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-		-- {0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0},
-		-- {0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0},
-		-- {0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0},
-		-- {0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,1,0,0,0,1,0,0,0,0,0},
+		{0,0,0,0,0,1,0,1,0,1,0,0,0,0,0},
+		{0,0,0,0,0,1,0,1,0,1,0,0,0,0,0},
+		{0,0,0,0,0,0,1,0,1,0,0,0,0,0,0},
+		{0,0,1,1,0,0,0,0,0,0,0,1,1,1,0},
+		{0,1,0,0,1,0,2,2,2,0,1,0,0,0,0},
+		{0,1,1,1,1,0,2,0,2,0,1,1,1,1,0},
+		{0,1,0,0,1,0,2,2,2,0,0,0,0,1,0},
+		{0,1,0,0,1,0,0,0,0,0,1,1,1,0,0},
+		{0,0,0,0,0,1,1,1,1,0,0,0,0,0,0},
+		{0,0,0,0,0,1,0,0,0,1,0,0,0,0,0},
+		{0,0,0,0,0,1,0,0,0,1,0,0,0,0,0},
+		{0,0,0,0,0,1,1,1,1,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 	}
-	map = {{currentMap}}
-	worldX = 1
-	worldY = 1
+	world = {{currentMap}} --all maps!
+	worldPos = {1,1} --x,y
 		
 	yLen = #(currentMap)
 	xLen = #(currentMap[1])
-	screenShiftingDirection = nil
+	screenShifting = nil
 	xOffsetCurrent = 0
 	yOffsetCurrent = 0
 	xOffsetNext = 0
@@ -70,94 +68,59 @@ function initTileSystem()
 	end
 	
 	updateTilesetBatchCurrent()
-	-- updateTilesetBatchNext()
 	
 	xMargin = 0
 	yMargin = 0
 end
 
-function triggerScreenShift(direction)
-	screenShiftingDirection = direction --?
-	-- if not screenShift then
-		if screenShiftingDirection == "east edge" then
-			worldX = worldX + 1 --possibly move this to the end of shift(), for triggering music changes or whatever
-			nextMap = getMap()
-			screenShiftingDirection = "map right"
-			xOffsetNext = xLen * tileSize
-			offsetCountdown = xOffsetNext
-		elseif screenShiftingDirection == "west edge" then
-			worldX = worldX - 1
-			nextMap = getMap()
-			screenShiftingDirection = "map left"
-			xOffsetNext = 0 - xLen * tileSize
-			offsetCountdown = math.abs(xOffsetNext) -- slightly hacky...
-		elseif screenShiftingDirection == "north edge" then
-			worldY = worldY - 1
-			nextMap = getMap()
-			screenShiftingDirection = "map up"
-			yOffsetNext = 0 - yLen * tileSize
-			offsetCountdown = math.abs(yOffsetNext) -- slightly hacky...
-		elseif screenShiftingDirection == "south edge" then
-			worldY = worldY + 1
-			nextMap = getMap()
-			screenShiftingDirection = "map down"
-			yOffsetNext = yLen * tileSize
-			offsetCountdown = yOffsetNext
-		else
-			screenShiftingDirection = nil
-		end
-		
-		updateTilesetBatchNext()
-	-- end
-end
+function triggerScreenShiftTo(tmi) --"target map index"
+	worldDest = tmi
+	nextMap = getMap(worldDest)
 
-function animateBG(dt)
-	timeBG = timeBG + dt
-	if timeBG > frameLength then
-		timeBG = 0
-		spriteState = (spriteState + 1) % 2
+	--moving horizontally or vertically?
+	if worldDest[1] == worldPos[1] then
+		yOffsetNext = (worldDest[2] - worldPos[2]) * yLen * tileSize
+	elseif worldDest[2] == worldPos[2] then
+		xOffsetNext = (worldDest[1] - worldPos[1]) * xLen * tileSize
+	else
+		print("something has gone very wrong in triggerScreenShiftTo()")
 	end
-end
 
-function drawBGTiles()
-	love.graphics.draw(tilesetBatchFramesCurrent[spriteState + 1], xOffsetCurrent + xMargin, yOffsetCurrent + yMargin)
-	if screenShiftingDirection then
-		love.graphics.draw(tilesetBatchFramesNext[spriteState + 1], xOffsetNext + xMargin, yOffsetNext + yMargin)
-	end
+	offsetCountdown = math.abs(xOffsetNext + yOffsetNext)
+	
+	updateTilesetBatchNext()
+	
+	screenShifting = true
 end
 
 function shiftTiles(dt)
-	o = (scrollSpeed * dt)
+	xDelta = (worldPos[1] - worldDest[1]) * scrollSpeed * dt
+	yDelta = (worldPos[2] - worldDest[2]) * scrollSpeed * dt
 	
-	if screenShiftingDirection == "map right" then
-		xOffsetNext = xOffsetNext - o
-		xOffsetCurrent = xOffsetCurrent - o
-	elseif screenShiftingDirection == "map left" then
-		xOffsetNext = xOffsetNext + o
-		xOffsetCurrent = xOffsetCurrent + o
-	elseif screenShiftingDirection == "map up" then
-		yOffsetNext = yOffsetNext + o
-		yOffsetCurrent = yOffsetCurrent + o
-	elseif screenShiftingDirection == "map down" then
-		yOffsetNext = yOffsetNext - o
-		yOffsetCurrent = yOffsetCurrent - o
-	end
+	xOffsetCurrent = xOffsetCurrent + xDelta
+	yOffsetCurrent = yOffsetCurrent + yDelta
+	xOffsetNext = xOffsetNext + xDelta
+	yOffsetNext = yOffsetNext + yDelta
 	
-	offsetCountdown = offsetCountdown - o
+	offsetCountdown = offsetCountdown - math.abs(xDelta + yDelta)
+	
 	if offsetCountdown <= 0 then
-		currentMap = nextMap
-		nextMap = nil
-		screenShiftingDirection = nil
-
-		xOffsetNext = 0
-		xOffsetCurrent = 0
-		yOffsetNext = 0
-		yOffsetCurrent = 0
-		offsetCountdown = 0
+		mapArrive()
 		updateTilesetBatchCurrent()
-		
-		-- print("ping")
 	end
+end
+
+function mapArrive()
+	currentMap = nextMap
+	worldPos = worldDest
+	nextMap = nil
+	screenShifting = nil
+
+	xOffsetNext = 0
+	xOffsetCurrent = 0
+	yOffsetNext = 0
+	yOffsetCurrent = 0
+	offsetCountdown = 0
 end
 
 function updateTilesetBatchCurrent()
@@ -181,37 +144,53 @@ function updateTilesetBatch(t, m)
 	end
 end
 
-function getMap()
-	if map[worldY] and map[worldY][worldX] then
-		return map[worldY][worldX]
+function getMap(tmi) --"target map index"
+	if world[tmi[2]] and world[tmi[2]][tmi[1]] then
+		return world[tmi[2]][tmi[1]]
 	else
-		if not map[worldY] then
-			map[worldY] = {}
+		if not world[tmi[2]] then
+			world[tmi[2]] = {}
 		end
 		m = makeRandomMap()
-		map[worldY][worldX] = m
+		world[tmi[2]][tmi[1]] = m
 		-- print("made a map at x="..worldX.." y="..worldY)
 		return m
 	end
 end
 
+function animateBG(dt)
+	timeBG = timeBG + dt
+	if timeBG > frameLength then
+		timeBG = 0
+		spriteState = (spriteState + 1) % 2
+	end
+end
+
+function drawBGTiles()
+	love.graphics.draw(tilesetBatchFramesCurrent[spriteState + 1], xOffsetCurrent + xMargin, yOffsetCurrent + yMargin)
+	if screenShifting then
+		love.graphics.draw(tilesetBatchFramesNext[spriteState + 1], xOffsetNext + xMargin, yOffsetNext + yMargin)
+		-- print("ping")
+	end
+end
+
 function makeRandomMap()
 	m = {}
-	if math.random() < 0.01 then -- tee hee
+	if math.random() < (score / 1000) then -- tee hee
 		m = {
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,1,1,0,0,1,0,0,1,1,1,0,0},
-			{0,0,1,0,0,0,1,0,1,0,0,1,0,0,0},
-			{0,0,1,0,1,0,1,0,1,0,0,1,0,0,0},
-			{0,0,0,1,1,0,0,1,0,0,0,1,0,0,0},
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,1,0,0,0,1,0,1,1,1,0,1,0,0},
-			{0,0,1,1,0,1,1,0,1,0,0,0,1,0,0},
-			{0,0,1,0,1,0,1,0,1,1,0,0,1,0,0},
-			{0,0,1,0,0,0,1,0,1,0,0,0,0,0,0},
-			{0,0,1,0,0,0,1,0,1,1,1,0,1,0,0},
+			{0,2,2,2,0,0,0,2,2,2,0,0,0,0,0},
+			{0,2,0,0,2,0,2,0,0,0,2,0,0,0,0},
+			{0,2,2,2,0,0,2,0,0,0,2,0,1,1,0},
+			{0,2,0,0,2,0,2,0,0,0,2,0,0,0,0},
+			{0,2,2,2,0,0,0,2,2,2,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,2,0,0,2,0,2,0,0,2,0,2,2,2,0},
+			{0,2,2,0,2,0,2,0,0,2,0,2,0,0,0},
+			{0,2,0,2,2,0,2,0,0,2,0,0,2,0,0},
+			{0,2,0,0,2,0,2,0,0,2,0,0,0,2,0},
+			{0,2,0,0,2,0,0,2,2,0,0,2,2,2,0},
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 		}
