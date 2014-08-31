24a6be9c31d0f3039272d38fda983852334357f4
Max W <xypotion@gmail.com>
2014-08-31 11:28:58 -0700

    quest 4! because i think it is time to add classes, which will be a major overhaul. wish me luck.

---
+++ quest4/pause.lua
@@ -0,0 +1,87 @@
+-- initializes and manages pause menu/overlay
+-- may eventually become the whole status menu controller, but we'll cross that bridge when we come to it
+
+function initPauseMenuSystem()
+	paused = false
+end
+
+function updatePauseScreen(dt)
+	tickAniKey(anikeys.minimap, dt)
+end
+
+function drawPauseOverlay()
+	--dark overlay
+  love.graphics.setColor(0, 0, 0, 100)
+  love.graphics.rectangle('fill', 0, 0, screenWidth, screenHeight)
+
+	love.graphics.setColor(255,255,255,255)
+	love.graphics.print("PAUSED", screenWidth/3, screenHeight/3, 0, zoom, zoom)
+end
+
+function togglePause()
+	if paused then
+		paused = false
+		-- TODO audio resume
+	else
+		paused = true
+		-- TODO audio pause
+	end
+end
+
+----------------------------------------------------------------------------------------------------
+-- TODO move below to menu manager of some kind. super doesn't belong here anymore
+
+function drawMapMenu()
+	--dark overlay
+  love.graphics.setColor(0, 0, 0, 100)
+  love.graphics.rectangle('fill', 0, 0, screenWidth, screenHeight)
+	
+	--"map" backdrop... pretty arbitrary, obvs TODO define & refine later ART NEEDED
+  love.graphics.setColor(191, 191, 127, 255)
+  love.graphics.rectangle('fill', screenWidth/4, screenHeight/4, screenHeight/2, screenHeight/2) --whatever for now TODO
+	
+	--the actual minimap
+	drawMiniMap({x = screenWidth/4, y = screenHeight/4}, 4)
+
+	-- and a helpful note ~
+	love.graphics.setColor(255,255,255,255)
+	love.graphics.print("TOGGLE MAP WITH M", (screenWidth - 150)/2, (screenHeight) - 26, 0, zoom, zoom)
+end
+
+function drawMiniMap(pos, scale)
+	local cellSize = 4 * zoom * scale
+	local cellGap = 1 * zoom * scale --maybe not necessary? i dunno
+	
+	for y = -10, 10 do
+		for x = -10, 10 do
+			if world[y] and world[y][x] then
+				if world[y][x].seen then
+					-- TODO replace colors with small images (by mapType) for each cell. don't worry about hackyness for now, it's gonna get scrapped. ART NEEDED
+					if world[y][x] == currentMap and anikeys.minimap.frame == 1 then
+						love.graphics.setColor(0,0,0,0) -- invisible, like imhotep
+					elseif world[y][x].mapType == "start" then 
+						love.graphics.setColor(223,223,255,255)
+					elseif world[y][x].mapType == "random" then 
+						love.graphics.setColor(95,223,95,255)
+					elseif world[y][x].mapType == "bonus" then 
+						love.graphics.setColor(223,31,223,255)
+					elseif world[y][x].mapType == "flat" then 
+						love.graphics.setColor(64,96,64,255)
+					elseif world[y][x].mapType == "hole" then 
+						love.graphics.setColor(0,0,0,255)
+					elseif world[y][x].mapType == "cave" then 
+						love.graphics.setColor(63,63,31,255)
+					else 
+						print("unknown map type encountered at "..x..", "..y)
+					end
+				else
+					love.graphics.setColor(127,127,127,255)
+				end
+
+				love.graphics.rectangle('fill', pos.x + x * (cellSize + cellGap), pos.y + y * (cellSize + cellGap), cellSize, cellSize)
+				-- or, centered on currentMap (have to also change bounds of y & x above):
+				-- love.graphics.rectangle('fill', (xLen * tileSize / 2) + (x - worldPos.x) * 10, (yLen * tileSize / 2) + (y - worldPos.y) * 10, 8, 8)
+			end
+		end
+	end
+end
---
+++ quest4/script/mapLoader.lua
@@ -0,0 +1,119 @@
+require "script/mapTileDataRaw"
+
+-- where map data is loaded at runtime and then fetched
+
+function loadWorld()
+	world = {}
+	
+	-- TODO use actual dimensions of world map. worldSize or whatever
+	-- TODO you keep thinking about Z positions for non-overworld maps, too. just implement it already!
+	for wy = 1,10 do
+		world[wy] = {}
+		
+		for wx = 1,10 do
+			world[wy][wx] = {}
+			insertMap(wx,wy)
+		end
+	end
+end
+
+--the big one. nothing else for it, really.
+-- TODO i mean is there any reason not to keep all this data in an external array and just call it from here? seems cleaner...
+function insertMap(wx,wy)
+	m = {}
+	-- m.events = emptyMapGrid()
+	-- m.eventShortcuts = {}
+	m.localActorPointers = {}
+	
+	if wx == 1 then
+		if wy == 1 then
+			m.tiles = mapTileDataRaw[1]
+			m.mapType = "start"
+			m.localActorPointers = {
+				-- {x=8,y=5,id=99} -- elf
+				{x=8,y=5,id=21}
+			}
+			m.fastTravelTargetPos = {x=8,y=12} 
+			m.seen = true -- derp TODO apply dynamically in or after saveLoader (then obvs also in navigation)
+		elseif wy == 2 then
+			m.tiles = mapTileDataRaw[2]
+			m.mapType = "bonus"
+			m.fastTravelTargetPos = {x=15,y=15}
+			m.seen = true
+		elseif wy == 3 then
+			m.tiles = mapTileDataRaw[3]
+			m.mapType = "cave"
+			m.fastTravelTargetPos = {x=15,y=15}
+			-- m.events[] -- ladder
+		end
+	elseif wx == 2 then
+		if wy == 1 then
+			m.tiles = mapTileDataRaw[5]
+			m.mapType = "hole"
+			m.localActorPointers = {
+				{x=8,y=3,id=100}, --rock OR hole to 1,3
+				{x=3,y=3,id=2}, --just hole
+				{x=13,y=13,id=101}, --rock2
+				{x=13,y=3,id=5},
+				{x=13,y=8,id=99}, --elf
+				{x=8,y=13,id=18},
+				{x=3,y=8,id=19}, --elf2
+			}
+			m.seen = true
+		elseif wy == 2 then
+			m.tiles = mapTileDataRaw[4]
+			m.mapType = "bonus"
+			m.localActorPointers = {
+				{x=3,y=8,id=6},
+				{x=13,y=8,id=7},
+				{x=3,y=3,id=20}, --!
+			}
+		elseif wy == 3 then
+			m.tiles = mapTileDataRaw[4]
+			m.mapType = "start"
+			m.localActorPointers = {
+				{x=2,y=3,id=8},
+				{x=5,y=3,id=9},
+				{x=8,y=3,id=10},
+				{x=11,y=3,id=11},
+				{x=14,y=3,id=12},
+				{x=2,y=5,id=13},
+				{x=5,y=5,id=14},
+				{x=8,y=5,id=15},
+				{x=11,y=5,id=16},
+				{x=14,y=5,id=17},
+			}
+		end
+	end
+
+	-- little catch-all for now. derp TODO will have no place in final game
+	if not m.tiles then
+		m.tiles = mapTileDataRaw[1]
+		m.mapType = "random"
+		m.localActorPointers = {
+			{x=8,y=5,id=6}
+		}
+	end
+
+	--TODO do without mixing chipset into .tiles in raw map data; it IS ok to have a default chipset, note
+	if m.tiles.chipset and m.tiles.chipset == 2 then
+		m.chipset = 2 --TODO think of something better to call this
+	else
+		m.chipset = 1
+	end
+
+	--...and put it in the world!
+	world[wy][wx] = m
+end
+
+-- map.tiles is an array of arrays; this just makes a blank one the same size as that (for something like .events)
+function emptyMapGrid()
+	t = {}
+	for y = 1,(yLen) do
+		t[y] = {}
+	end
+
+	return t
+end
+
+rawTileArray = {}
