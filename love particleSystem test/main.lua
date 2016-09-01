5e976df55934fac8a22406bb1c1d2f6e36a93ad9
Max W <xypotion@gmail.com>
2016-09-01 15:55:30 -0700

    more experimenting with love's ParticleSystem. some of it is nice... not sure which i like better now :/

--- love particleSystem test/main.lua
+++ love particleSystem test/main.lua
@@ -1,15 +1,32 @@
 function love.load()
-	local bubble = love.graphics.newImage("bubble.png")
+	counter = 0
+	
+	local bubble = love.graphics.newImage("big bubble.png") -- http://www.freeiconspng.com/uploads/bubble-png-9.png
 	psystem = love.graphics.newParticleSystem(bubble, 100)
 
 	psystem:setParticleLifetime(2, 9)
-	psystem:setEmissionRate(50)
+	psystem:setEmissionRate(5)
 	psystem:setSizeVariation(0.5)
+	-- psystem:setSpeed(50)
 	psystem:setLinearAcceleration(-120, -20, 20, 20) -- Random movement in all directions.
-	psystem:setColors(255, 255, 255, 255, 0, 0, 255, 0) -- Fade to transparency.
+	psystem:setColors(255, 255, 255, 0, 255, 255, 255, 255) -- Fade to transparency.
 	
 	psystem:setTangentialAcceleration(0, 100)
 	-- psystem:setRadialAcceleration(0, 100)
+	
+	-- psystem:setSpin(0, 10)
+	psystem:setSizes(0.015625, 0.125)
+	
+	-- psystem:setQuads(
+	--   love.graphics.newQuad(0,0,8,8,16,16),
+	--   love.graphics.newQuad(0,8,8,8,16,16),
+	--   love.graphics.newQuad(0,0,8,8,16,16),
+	--   love.graphics.newQuad(0,8,8,8,16,16),
+	--   love.graphics.newQuad(0,0,8,8,16,16),
+	--   love.graphics.newQuad(0,8,8,8,16,16),
+	--   love.graphics.newQuad(0,0,8,8,16,16),
+	--   love.graphics.newQuad(0,8,8,8,16,16)
+	-- )
 end
  
 function love.draw()
@@ -18,5 +35,19 @@ function love.draw()
 end
  
 function love.update(dt)
+	if paused then return end
+	
+	-- counter = counter + dt
+	-- psystem:moveTo(counter, 300)
+	
 	psystem:update(dt)
+	-- psystem:update(dt) --double
+end
+
+function love.keypressed(key)
+	if key == "escape" then
+		love.event.quit()
+	elseif key == "space" then
+		paused = not paused
+	end
 end
